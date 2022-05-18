3f609a7d60ca56c9fb69f228ec6308d7868332fe
xk999 <xeniak91@yandex.ru>
2022-05-18 21:51:24 +0500

    update test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -2,9 +2,6 @@ from pages.main_page import MainPage # точка перед pages убрана,
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-'''def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()'''
 def test_guest_can_go_to_login_page(browser): 
     page = MainPage(browser, link)
     page.open()
