ebd93d96213f2d6a3451bddc2899bfd4aea76a0c
xk999 <xeniak91@yandex.ru>
2022-05-18 21:49:30 +0500

    update main_page and test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+from pages.main_page import MainPage # точка перед pages убрана, т. к. она вызывает ошибку импорта (no known parent package)
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+'''def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    login_link.click()'''
+def test_guest_can_go_to_login_page(browser): 
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()     
