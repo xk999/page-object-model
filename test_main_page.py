a3ec2cf9ab10a4407dea65f47171f7601eb9122c
xk999 <xeniak91@yandex.ru>
2022-05-18 23:43:54 +0500

    add element presence check

--- test_main_page.py
+++ test_main_page.py
@@ -5,4 +5,9 @@ link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_can_go_to_login_page(browser): 
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()     
+    page.go_to_login_page()    
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
