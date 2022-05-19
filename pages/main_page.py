0f197123c8f233789dd332a37f02d0b353773eb2
xk999 <xeniak91@yandex.ru>
2022-05-19 15:37:45 +0500

    add locators and test for login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,12 @@
 from .base_page import BasePage
+from .login_page import LoginPage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click() 
+        # return LoginPage(browser=self.browser, url=self.browser.current_url) 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"    
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"    
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,25 @@
 from pages.main_page import MainPage # точка перед pages убрана, т. к. она вызывает ошибку импорта (no known parent package)
+from pages.login_page import LoginPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser): 
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()    
+    page.go_to_login_page() 
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()   
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_forms(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() 
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
