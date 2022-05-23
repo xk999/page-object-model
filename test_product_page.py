d5b021a4a6f2d19ad375c2c92dfcf520ee8f9a15
xk999 <xeniak91@yandex.ru>
2022-05-23 18:11:31 +0500

    add marks and update requirements

--- test_product_page.py
+++ test_product_page.py
@@ -7,6 +7,7 @@ import pytest
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser): 
     page = ProductPage(browser, link)
     page.open()
@@ -40,11 +41,13 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):   
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -67,6 +70,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser): 
         page = ProductPage(browser, link)
         page.open()
